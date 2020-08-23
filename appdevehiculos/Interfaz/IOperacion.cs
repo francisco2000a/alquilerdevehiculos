0c1cd19c018bba5e845ae4f2519cf4d8f6014c1b
Byron_Pilligua1998 <68709836+byronpilligua@users.noreply.github.com>
2020-08-22 21:13:38 -0500

    agregando patron con correcion de erick

---
+++ appdevehiculos/Interfaz/IOperacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appdevehiculos.Interfaz
+{
+    // Creacion de la interfaz para utilizar el patron strategy.
+    interface IOperacion
+    {
+        List<Vehiculo> listarVehiculo();
+        Boolean registrarVehiculo(Vehiculo vehiculo);
+    }
+}
