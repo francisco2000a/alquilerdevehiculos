2e1fc751c64cd12406e2607ef47208a32e26bb37
erickmera2020 <68703330+erickmera2020@users.noreply.github.com>
2020-08-22 21:25:55 -0500

    subiendo el codigo del patron comentado por lineas

---
+++ appdevehiculos/Vehiculo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appdevehiculos
+{
+
+    public class Vehiculo
+    {
+        private string matricula;
+        private string modelo;
+        private string marca;
+        private string color;
+        private double precio_alquiler_dias;
+
+        public Vehiculo(string matricula, string modelo, string marca, string color, double precio)
+        {
+            this.matricula = matricula;
+            this.modelo = modelo;
+            this.marca = marca;
+            this.color = color;
+            this.precio_alquiler_dias = precio;
+        }
+
+        public Vehiculo()
+        {
+        }
+
+        public string getMatricula()
+        {
+            return matricula;
+        }
+
+        public void setMatricula(string matricula)
+        {
+            this.matricula = matricula;
+        }
+
+        public string getModelo()
+        {
+            return modelo;
+        }
+
+        public void setModelo(string modelo)
+        {
+            this.modelo = modelo;
+        }
+
+        public string getMarca()
+        {
+            return marca;
+        }
+
+        public void setMarca(string marca)
+        {
+            this.marca = marca;
+        }
+
+        public string getColor()
+        {
+            return color;
+        }
+
+        public void setColor(string color)
+        {
+            this.color = color;
+        }
+
+        public double getPrecioDia()
+        {
+            return precio_alquiler_dias;
+        }
+
+        public void setPrecio(double precio)
+        {
+            this.precio_alquiler_dias = precio;
+        }
+    }
+}
