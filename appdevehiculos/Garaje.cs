db62bf0a05f8e26b48bcca6c251bf6bcc1ca10d9
francisco2000a <68514732+francisco2000a@users.noreply.github.com>
2020-09-06 20:20:20 -0500

    "agregando exprecion al program"

--- appdevehiculos/Garaje.cs
+++ appdevehiculos/Garaje.cs
@@ -6,15 +6,13 @@ namespace appdevehiculos
 {
     class Garaje : Lista_Cliente
     {
-        public Garaje(int cod_Garaje, String nombre_garaje)
+        public Garaje(int cod_Garaje, string nombre_garaje)
         {
             this.Cod_Garaje = cod_Garaje;
             this.Nombre_Garaje = nombre_garaje;
         }
         public int Cod_Garaje { get; private set; }
-        public String Nombre_Garaje { get; set; }
-
-
-
+        public string Nombre_Garaje { get; set; }
+        
     }
 }
---
+++ appdevehiculos/Interfaz/ICliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appdevehiculos.Interfaz
+{
+    interface ICliente
+    {
+        Boolean addClient(Cliente cliente);
+        Boolean updateClient(string cedula, Cliente cliente);
+        void findByClient(string cedula);
+        void listClient();
+    }
+}
